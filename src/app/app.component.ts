9953197a2d2a2318eafbdb5669e495b6135cc552
Matthew Haugen <mhaugen@haugenapplications.com>
2024-02-22 14:03:07 -0800

    add randomized, playful affirmations and negations to keep things fresh

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,51 @@
 import { CommonModule } from "@angular/common";
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Observable } from "rxjs";
+import { Observable, distinctUntilChanged, map } from "rxjs";
 import { BingoBoardComponent } from "./components/bingo-board/bingo-board.component";
 import { GameStateService, IBingoTile, ITileCell } from "./services/game-state.service";
+
+interface IPreviewCellHeader {
+  cell: ITileCell;
+  affirmation: string;
+  negation: string;
+}
+
+const affirmations = [
+  "Absolutely!",
+  "You know it!",
+  "That's right!",
+  "Yes, indeed!",
+  "For sure!",
+  "Yep, that's me!",
+  "Totally me!",
+  "Match!",
+  "Affirmative!",
+  "All me!",
+  "Right here!",
+  "No doubt!"
+];
+
+const negations = [
+  "Oops, not me!",
+  "Just kidding!",
+  "Take it back!",
+  "Changed my mind!",
+  "Mistaken click!",
+  "Not quite right!",
+  "Hold on, not that one!",
+  "Wait, maybe not!",
+  "Nope, scratch that!",
+  "On second thought, no.",
+  "I spoke too soon!",
+  "Actually, maybe not.",
+  "My bad, uncheck!",
+  "Let's reverse that!",
+  "Err... nope!",
+  "Rewind that!",
+  "I take it back!",
+  "Disregard!"
+];
 
 @Component({
   selector: 'app-root',
@@ -13,11 +55,25 @@ import { GameStateService, IBingoTile, ITileCell } from "./services/game-state.s
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  readonly previewCell$: Observable<ITileCell | null>;
+  readonly previewCell$: Observable<IPreviewCellHeader | null>;
   readonly hasBingo$: Observable<boolean>;
   constructor(private readonly gameState: GameStateService) {
-    this.previewCell$ = gameState.preview$;
     this.hasBingo$ = gameState.hasBingo$;
+
+    this.previewCell$ = gameState.preview$.pipe(
+      distinctUntilChanged((a, b) => !!a && !!b && a.id === b.id),
+      map((cell): IPreviewCellHeader | null => {
+        if (!cell) {
+          return null;
+        }
+
+        return {
+          cell,
+          affirmation: affirmations[Math.floor(Math.random() * affirmations.length)],
+          negation: negations[Math.floor(Math.random() * negations.length)]
+        }
+      })
+    );
   }
 
   toggleTile(tile: IBingoTile) {
